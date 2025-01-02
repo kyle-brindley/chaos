4f9bed780fb12d8201e603be4207f52ffa571266
Kyle Brindley <kyle.brindley@gmail.com>
2025-01-02 12:27:14 -0700

    TST: test the plot vs. save behavior

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 import main
@@ -27,3 +29,23 @@ import main
 )
 def test_logistic(x_current, r, x_next_expected):
     assert main.logistic(x_current, r) == x_next_expected
+
+
+def test_plot_states():
+    # No output should raise plot window
+    with (
+        patch("matplotlib.pyplot.savefig") as mock_save_fig,
+        patch("matplotlib.pyplot.show") as mock_show,
+    ):
+        main.plot_states([[1.0]], 1.0, output=None)
+        mock_save_fig.assert_not_called()
+        mock_show.assert_called_once()
+
+    # Output should save the figure without plot window
+    with (
+        patch("matplotlib.pyplot.savefig") as mock_save_fig,
+        patch("matplotlib.pyplot.show") as mock_show,
+    ):
+        main.plot_states([[1.0]], 1.0, output="dummy.png")
+        mock_save_fig.assert_called_once()
+        mock_show.assert_not_called()
