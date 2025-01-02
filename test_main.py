60b7ac5182d63c49244e44e2ef6995e113620299
Kyle Brindley <kyle.brindley@gmail.com>
2025-01-02 09:24:43 -0700

    TST: add some trivial cases of the logistic function to unit tests

--- test_main.py
+++ test_main.py
@@ -6,7 +6,12 @@ import main
 @pytest.mark.parametrize(
     "x_current, r, x_next_expected",
     [
-        (0., 1., 0.)
+        (0., 0., 0.),
+        (0., 1., 0.),
+        (0., 10., 0.),
+        (1., 0., 0.),
+        (10., 0., 0.),
+        (1., 1., 0.),
     ],
 )
 def test_logistic(x_current, r, x_next_expected):
