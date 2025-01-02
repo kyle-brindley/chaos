b9cf57f8b504ec9413abba71333eea70d4918b99
Kyle Brindley <kyle.brindley@gmail.com>
2025-01-02 09:20:37 -0700

    FEAT: add logistic function

---
+++ main.py
@@ -0,0 +1,19 @@
+def logistic(x: float, r: float):
+    r"""Return the next value in the logistic function give the current value
+
+    .. math
+
+       x_{next} = r x_{current} \left ( 1 - x_{current} \right )
+
+    :param x: The current :math:`x_{current}` value of the logistic function
+    :param r: The parameter :math:`r` value of the logistic function
+    """
+    return r * x * (1 - x)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
