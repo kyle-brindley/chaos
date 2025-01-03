acd334c9f97d7770af5b8e3c1006432dc20af587
Kyle Brindley <kyle.brindley@gmail.com>
2025-01-02 22:38:38 -0700

    WIP: set defaults to better capture the r~=1 transition

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot
 DESCRIPTION = """Calculate and plot the logistic function:
 `x_{next} = r * x_{current} * (1 - x_{current})`"
 """
-DEFAULT_MAX_ITERATION = 100
-DEFAULT_RELATIVE_TOLERANCE = 1e-4
+DEFAULT_MAX_ITERATION = 200
+DEFAULT_RELATIVE_TOLERANCE = 1e-6
 DEFAULT_MAX_PERIOD = 12
 
 
